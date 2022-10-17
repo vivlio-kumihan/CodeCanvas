fca1fe7eb2cdabbc728b706d017557a2a4151b28
Nobuyuki Takahiro <studio.quad9@gmail.com>
2022-10-17 13:36:03 +0900

    レイアウト作成

---
+++ position/assets/js/behavior.js
@@ -0,0 +1,21 @@
+// hamburger menu
+
+const menu = document.querySelector(".gnav_1")
+const button = document.querySelector(".btn")
+const overlay = document.querySelector(".overlay")
+const body = document.body
+
+const toggleMenu = () => {
+  menu.classList.toggle("menuIsOpen")
+  overlay.classList.toggle("overlayIsOpen")
+  button.classList.toggle("btnIsOpen")
+  body.classList.toggle("isScrollAllowed")
+}
+
+  button.addEventListener("click", () => {
+  toggleMenu()
+})
+
+  overlay.addEventListener("click", () => {
+  toggleMenu()
+})
