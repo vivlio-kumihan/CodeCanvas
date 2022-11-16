a0133c22c268e2f0041ff3edf18362a4cc889ef2
Nobuyuki Takahiro <studio.quad9@gmail.com>
2022-11-17 04:50:11 +0900

    変更をコミットする。

--- ナビゲーション/assets/js/behavior.js
+++ ナビゲーション/assets/js/behavior.js
@@ -1,6 +1,6 @@
 // hamburger menu
 
-const menu = document.querySelector(".gnav_1")
+const menu = document.querySelector(".gnav_3")
 const button = document.querySelector(".btn")
 const overlay = document.querySelector(".overlay")
 const body = document.body
