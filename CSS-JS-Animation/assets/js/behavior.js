844560b2bc4dfa1a17b7524951e99288530e61e0
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-05 15:34:24 +0900

    CSS-JS-Animation => transform属性と色々なtranslateプロパティ topics:菱形を変形させて見出しの飾りを作る
    	回転
    		transform: rotate(45deg)
    	拡大・縮小
    		transform: scale(2, .5)
    	歪
    		transform: skewY(-45deg)

--- CSS-JS-Animation/assets/js/behavior.js
+++
@@ -1,5 +0,0 @@
-const btn = document.querySelector('input[type="button"]')
-btn.addEventListener("click", function() {
-  const info = document.querySelector("dl > .wrapper:first-of-type > dd")
-  info.classList.toggle("active")
-}, false)
