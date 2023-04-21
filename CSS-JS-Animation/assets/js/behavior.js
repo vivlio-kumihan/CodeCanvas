893578664c43cb3bde48bcdf8db3b433fde45918
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-21 09:44:08 +0900

    CSS-JS-Animation => tips:HTML上にanchorを見せず、data-XXXX属性で設定しJS上でリンクを設定する

--- CSS-JS-Animation/assets/js/behavior.js
+++ CSS-JS-Animation/assets/js/behavior.js
@@ -1,13 +1,19 @@
-const openMenu = document.getElementById("open-menu")
- openMenu.addEventListener("click", function() {
-  this.lastElementChild.classList.toggle("active")
-})
-
-// menuあたりをマウスがホバーしたらのアクション
-// const menuList = document.getElementById("menuList")
-// menuList.addEventListener("mouseenter", function() {
-//   this.classList.add("active")
-// })
-// menuList.addEventListener("mouseleave", function() {
-//   this.classList.remove("active")
-// })
+// HTML上にanchorを見せず、data-value属性で設定しJS上でリンクを設定する。
+const dl = document.querySelectorAll(".wrapper dl")
+console.log(dl)
+for (let anchor of dl) {
+  anchor.addEventListener("click", function() {
+    let suffix = this.dataset.suffix
+    switch (suffix) {
+      case "mac":
+        location.href = `https://www.apple.com/jp/${ suffix }`
+        break;
+      case "ipad":
+        location.href = `https://www.apple.com/jp/${ suffix }`
+        break;
+      case "iphone":
+        location.href = `https://www.apple.com/jp/${ suffix }`
+        break;
+    }
+  })
+}
