24f43bb08824dc6308ef513f273135b29e3f37a8
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-24 15:26:29 +0900

    CSS-JS-Animation => アニメーションの設定項目
    	CSS Animation 設定項目
    		name              呼び出すキーフレームの名前
    		duration          アニメーションの継続時間
    		timing-function   どのような加速感を持った関数を呼び出すか
    		delay             アニメーションの開始時間
    		iteration-count   何回繰り返すか
    		direction         アニメーションの方向
    		fill-mode         初期・始点・終点時のオブジェクトのCSS
    		play-state        一旦停止を指示

--- CSS-JS-Animation/assets/js/behavior.js
+++ CSS-JS-Animation/assets/js/behavior.js
@@ -1,19 +1,5 @@
-// HTML上にanchorを見せず、data-value属性で設定しJS上でリンクを設定する。
-const dl = document.querySelectorAll(".wrapper dl")
-console.log(dl)
-for (let anchor of dl) {
-  anchor.addEventListener("click", function() {
-    let suffix = this.dataset.suffix
-    switch (suffix) {
-      case "mac":
-        location.href = `https://www.apple.com/jp/${ suffix }`
-        break;
-      case "ipad":
-        location.href = `https://www.apple.com/jp/${ suffix }`
-        break;
-      case "iphone":
-        location.href = `https://www.apple.com/jp/${ suffix }`
-        break;
-    }
-  })
-}
+const btn = document.querySelector("input")
+btn.addEventListener("click", function() {
+  const figure = this.previousElementSibling.firstElementChild
+  figure.classList.toggle("pause")
+})
