79e325909eb80a05f4a49fb97f7ba90b44c8134c
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-25 15:06:03 +0900

    CSS-JS-Animation => ローディング・アニメーション
    	ネタとしてローディング・アニメーションを持っておく必要はあるが、
    	せいぜい5ヶ月間で10個程度を揃えるくらいで構わない。
    	それよりもシステムが延滞している状態でローディングできるよう
    	JSで連携を取れる仕組みづくりを学習すべき。

--- CSS-JS-Animation/assets/js/behavior.js
+++ CSS-JS-Animation/assets/js/behavior.js
@@ -1,5 +1,14 @@
-const btn = document.querySelector("input")
-btn.addEventListener("click", function() {
-  const figure = this.previousElementSibling.firstElementChild
-  figure.classList.toggle("pause")
+// ローディング画面をクリックしたら開ける
+// window.addEventListener("load", function() {
+//   const loadingBg = document.getElementById("loading-bg")
+//   loadingBg.addEventListener("click", function() {
+//     this.classList.remove("active")
+//   })
+// })
+
+// システムが何かしているのをsetTimeout関数を使って仮装する
+window.addEventListener("load", function() {
+  this.setTimeout(() => {
+    document.getElementById("loading-bg").classList.remove("active")
+  }, 8000)
 })
