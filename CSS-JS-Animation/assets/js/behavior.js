0e39a6fb91e82f8fb395e60aa7e9397375480d11
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-28 15:40:44 +0900

    CSS-JS-Animation => スクロールで要素をフワっとアニメーション Basic ver transform:, opacity, visibility

--- CSS-JS-Animation/assets/js/behavior.js
+++ CSS-JS-Animation/assets/js/behavior.js
@@ -1,26 +1,55 @@
-setTimeout(function() {
-  location.href = "#one"
-}, 2000)
+// スクリーンの下辺から500pxの距離に要素の上辺が触れたら表示する。
 
-const anchorToSection = Array.from(document.getElementById("anchor-to-section").children)
-anchorToSection.forEach(function(elem) {
-  elem.addEventListener("click", function(e) {
-    switch(e.target.className) {
-      case "one":
-        location.href = "#one"
-        break;
-      case "two":
-        location.href = "#two"
-        break;
-      case "three":
-        location.href = "#three"
-        break;
-      case "four":
-        location.href = "#four"
-            break;
-      case "five":
-        location.href = "#five"
-        break;
+// const targetElems = Array.from(document.querySelectorAll(".animation-target"))
+// const setWindowHight = window.innerHeight - 500
+
+// document.addEventListener("scroll", function() {
+//   targetElems.forEach(function(elem, idx) {
+//     // ブラウザの上辺から要素の上辺までの距離
+//     // console.log(elem.getBoundingClientRect())
+//     let getElemDistance = elem.getBoundingClientRect().top
+//     if (setWindowHight > getElemDistance) {
+//       elem.classList.add("active")
+//     }
+//   })
+// })
+
+
+// 要素の半分の高さがスクリーンに現れた地点で表示する。
+// ただし、縦に長すぎる要素の場合は効果が効いていないような場合もある。
+// ケースバーケースで敷設方法を考える。
+
+const targetElems = Array.from(document.querySelectorAll(".animation-target"))
+const windowHight = window.innerHeight
+
+document.addEventListener("scroll", function() {
+  targetElems.forEach(function(elem) {
+    const haflElemHight = elem.clientHeight / 2
+    let setHight = windowHight - haflElemHight
+    let getElemDistance = elem.getBoundingClientRect().top
+    if (setHight > getElemDistance) {
+      elem.classList.add("active")
     }
   })
 })
+targetElems.forEach(function(elem) {
+
+})
+
+
+
+const setWindowHight = window.innerHeight - 500
+
+// document.addEventListener("scroll", function() {
+//   targetElems.forEach(function(elem, idx) {
+//     // ブラウザの上辺から要素の上辺までの距離
+//     // console.log(elem.getBoundingClientRect())
+//     let getElemDistance = elem.getBoundingClientRect().top
+//     if (setWindowHight > getElemDistance) {
+//       elem.classList.add("active")
+//     }
+//   })
+// })
+
+
+
