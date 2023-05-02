72c71f721b61442782d394b8af41092998126cf1
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-05-03 07:48:30 +0900

    CSS-JS-Animation => カーテン開閉アニメーション

--- CSS-JS-Animation/assets/js/behavior.js
+++ CSS-JS-Animation/assets/js/behavior.js
@@ -1,29 +1,11 @@
-// スクリーンの下辺から500pxの距離に要素の上辺が触れたら表示する。
-
-// const targetElems = Array.from(document.querySelectorAll(".animation-target"))
-// const setWindowHight = window.innerHeight - 500
-
-// document.addEventListener("scroll", function() {
-//   targetElems.forEach(function(elem, idx) {
-//     // ブラウザの上辺から要素の上辺までの距離
-//     // console.log(elem.getBoundingClientRect())
-//     let getElemDistance = elem.getBoundingClientRect().top
-//     if (setWindowHight > getElemDistance) {
-//       elem.classList.add("active")
-//     }
-//   })
-// })
-
-
-// 要素の半分の高さがスクリーンに現れた地点で表示する。
-// ただし、縦に長すぎる要素の場合は効果が効いていないような場合もある。
-// ケースバーケースで敷設方法を考える。
-
+// const targetElems = document.querySelectorAll(".animation-target")
+// 収集した要素はNodeListだったのでforEachは何も細工しなくてもいける。
+// ただ、いちいちHTMLCollectionかどうか見るのも邪魔くさいので。。。どっちがスマートかな？
 const targetElems = Array.from(document.querySelectorAll(".animation-target"))
 const windowHight = window.innerHeight
 
-document.addEventListener("scroll", function() {
-  targetElems.forEach(function(elem) {
+document.addEventListener("scroll", function () {
+  targetElems.forEach(elem => {
     const haflElemHight = elem.clientHeight / 2
     let setHight = windowHight - haflElemHight
     let getElemDistance = elem.getBoundingClientRect().top
@@ -32,24 +14,3 @@ document.addEventListener("scroll", function() {
     }
   })
 })
-targetElems.forEach(function(elem) {
-
-})
-
-
-
-const setWindowHight = window.innerHeight - 500
-
-// document.addEventListener("scroll", function() {
-//   targetElems.forEach(function(elem, idx) {
-//     // ブラウザの上辺から要素の上辺までの距離
-//     // console.log(elem.getBoundingClientRect())
-//     let getElemDistance = elem.getBoundingClientRect().top
-//     if (setWindowHight > getElemDistance) {
-//       elem.classList.add("active")
-//     }
-//   })
-// })
-
-
-
