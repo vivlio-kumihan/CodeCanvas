919028cc3cc71e3cd76b3cff5e4086db3dbbd503
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-05-09 16:35:31 +0900

    CSS-JS-Animation => Headerを上にスクロールで出現、下にスクロールで収納

--- CSS-JS-Animation/assets/js/behavior.js
+++ CSS-JS-Animation/assets/js/behavior.js
@@ -1,16 +1,12 @@
-// const targetElems = document.querySelectorAll(".animation-target")
-// 収集した要素はNodeListだったのでforEachは何も細工しなくてもいける。
-// ただ、いちいちHTMLCollectionかどうか見るのも邪魔くさいので。。。どっちがスマートかな？
-const targetElems = Array.from(document.querySelectorAll(".animation-target"))
-const windowHight = window.innerHeight
-
-document.addEventListener("scroll", function () {
-  targetElems.forEach(elem => {
-    const haflElemHight = elem.clientHeight / 2
-    let setHight = windowHight - haflElemHight
-    let getElemDistance = elem.getBoundingClientRect().top
-    if (setHight > getElemDistance) {
-      elem.classList.add("active")
-    }
-  })
+const header = document.querySelector("header")
+scrollValue = 0
+window.addEventListener("scroll", function() {
+  // アップ
+  if (scrollValue < this.scrollY) {
+    header.classList.add("active")
+    // ダウン
+  } else {
+    header.classList.remove("active")
+  }
+  scrollValue = this.scrollY
 })
