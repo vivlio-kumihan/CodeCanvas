9605d219cf175e9604a000c9bf0f4e63a415115b
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-05-12 14:51:58 +0900

    CSS-JS-Animation => モーダルウィンドウ改良版 詰まったので質問をする。
    	問題の解法
    		この問題は、close-btn要素に対するイベントリスナーが正しく設定されていないためです。close-btn要素に対してイベントリスナーを設定する際に、親要素であるdd要素が対象となっているため、e.target.parentElementで親要素のdd要素を取得していますが、この親要素にはactive-modalクラスが設定されていないため、クラスの削除ができず、モーダルウィンドウが閉じない状態が続いています。
    
    		修正するためには、close-btn要素に直接イベントリスナーを設定する必要があります。closeBtns.forEach()のコールバック関数内で、e.stopPropagation()を実行して親要素であるdd要素に対するクリックイベントの伝播を止め、closeBtn要素のみに対するクリックイベントを発生させます。以下は修正後のコードです。

--- CSS-JS-Animation/assets/js/behavior.js
+++ CSS-JS-Animation/assets/js/behavior.js
@@ -1,28 +1,63 @@
-const btn = document.getElementById("modal-open")
-const contents = document.querySelector(".modal")
-const fig = document.querySelector(".mask")
-const closeBtn = document.querySelector(".modal > button")
+// // ///////////////////////////////////////////////////////// sample 01
+// const btn = document.getElementById("modal-open")
+// const contents = document.querySelector(".modal")
+// const fig = document.querySelector(".mask")
+// const closeBtn = document.querySelector(".modal > button")
 
-btn.addEventListener("click", function() {
-  contents.classList.add("cover")
-  fig.classList.add("cover")
-})
-
-closeBtn.addEventListener("click", function() {
-  contents.classList.remove("cover")
-  fig.classList.remove("cover")
-})
-
-
-
-
-// const buttons = Array.from(document.querySelectorAll(".section"))
-// const mask = getComputedStyle(document.querySelector("article", "::after"))
-// console.log(mask)
-
-// buttons.forEach(btn => {
-//   btn.addEventListener("click", function() {
-//     this.lastElementChild.classList.toggle("active-modal")
-//     mask.classList.toggle("active-modal")
-//   })  
+// btn.addEventListener("click", function() {
+//   contents.classList.add("cover")
+//   fig.classList.add("cover")
 // })
+
+// closeBtn.addEventListener("click", function() {
+//   contents.classList.remove("cover")
+//   fig.classList.remove("cover")
+// })
+
+// 【やりたい事】
+// 一つの領域に複数のボタンがあり、それぞれにモーダルウィンドウを実装させたい。
+// モーダルウィンドウを開くことはできたが、閉じることが出来ない。根本的に作り方に問題があるのか？
+
+///////////////////////////////////////////////////////// sample 02
+const buttons = Array.from(document.querySelectorAll("dl"))
+const mask = document.getElementById("modal-mask")
+const closeBtn = document.querySelector(".close-btn")
+
+// dl要素を全て収集して、特定のdlへのクリックをトリガーにして任意の要素に対してクラスを付与する作戦。
+// これは理解。
+buttons.forEach(btn => {
+  btn.addEventListener("click", function() {
+    this.lastElementChild.classList.add("active-modal")
+    mask.classList.add("active-modal")
+  })
+})
+
+// ここから質問です。
+// 想定した挙動にならないです。
+// モーダルウィンドウが開いている状態で、モーダルウィンドウ上にあるボタンをクリックすることで、
+// 任意の要素に対してクラスを解除する作戦。上手くいかないです。
+
+// モーダルウィンドウ上にあるボタンをクリックする。
+closeBtn.addEventListener("click", function (e) {
+  // 親要素を捕まえたい。
+  console.log(e.target.parentElement) // => <dd class="active-modal">　掴めてる。
+  // 親要素のクラスを確認する。
+  console.log(e.target.parentElement.classList) //=> DOMTokenList [ "active-modal" ] 確認できた。
+
+  // ///////////////////////////////////////////////////////
+  // クラスの削除を実行する。
+  e.target.parentElement.classList.remove("active-modal")
+  // ///////////////////////////////////////////////////////
+
+  // クラスの削除を確認する。
+  console.log(e.target.parentElement) //=> <dd class=""> コンソール上では確認できた。でもHTMLに反映されない。
+  
+  // 以下も同様。
+  console.log(mask) //=> <figure id="modal-mask" class="active-modal">　そこに居ることを確認できた。
+
+  // ///////////////////////////////////////////////////////
+  mask.classList.remove("active-modal")
+  // ///////////////////////////////////////////////////////
+
+  console.log(mask) //=> <figure id="modal-mask" class=""> コンソール上では確認できた。でもHTMLに反映されない。。
+})
